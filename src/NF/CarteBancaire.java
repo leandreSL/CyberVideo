766c9915b72cb1d62b3bc8a50fe3e8406f634248
Antoine Roffet <roffeta@f204-07.e-im2ag.ujf-grenoble.fr>
2019-11-04 15:06:06 +0100

    nf_fin_de_seance

--- src/NF/CarteBancaire.java
+++ src/NF/CarteBancaire.java
@@ -1,6 +1,9 @@
 package NF;
 
+import java.util.Date;
+
 public class CarteBancaire {
 	private String nom;
-	private long numeroCarte; //Verifier si 
+	private long numeroCarte; //Verifier si type assez long
+	private Date dateExpiration;
 }
--- src/NF/Emprunteur.java
+++ src/NF/Emprunteur.java
@@ -1,6 +1,6 @@
 package NF;
 
 public class Emprunteur {
-
+	CarteBancaire cb;
 	
 }
