908da2d1d346bae95b8acc550afa82ac0f6096b9
Antoine Roffet <roffeta@f214-06.e-im2ag.ujf-grenoble.fr>
2019-11-19 15:57:43 +0100

    Getter Setter Abonne Emprunteur CB

--- src/NF/Abonne.java
+++ src/NF/Abonne.java
@@ -6,9 +6,42 @@ public class Abonne extends Emprunteur{
 	String nomAbonne;
 	String prenomAbonne;
 	List<Genre> restrictions;
+	int carte;
 	
 	void recommanderFilm(Film F) {
 		
+	}
+
+	public Abonne(CarteBancaire cb, String nomAbonne, String prenomAbonne, List<Genre> restrictions, int carte) {
+		super(cb);
+		this.nomAbonne = nomAbonne;
+		this.prenomAbonne = prenomAbonne;
+		this.restrictions = restrictions;
+		this.carte = carte;
+	}
+
+	public String getNomAbonne() {
+		return nomAbonne;
+	}
+
+	public void setNomAbonne(String nomAbonne) {
+		this.nomAbonne = nomAbonne;
+	}
+
+	public String getPrenomAbonne() {
+		return prenomAbonne;
+	}
+
+	public void setPrenomAbonne(String prenomAbonne) {
+		this.prenomAbonne = prenomAbonne;
+	}
+
+	public List<Genre> getRestrictions() {
+		return restrictions;
+	}
+
+	public void setRestrictions(List<Genre> restrictions) {
+		this.restrictions = restrictions;
 	}
 	
 }
--- src/NF/Emprunteur.java
+++ src/NF/Emprunteur.java
@@ -3,8 +3,23 @@ package NF;
 public class Emprunteur {
 	CarteBancaire cb;
 	
-	
-	void reserverFilm() {
+	void louerFilm() {
 		
 	}
+
+	public Emprunteur(CarteBancaire cb) {
+		super();
+		this.cb = cb;
+	}
+
+	public CarteBancaire getCb() {
+		return cb;
+	}
+
+	public void setCb(CarteBancaire cb) {
+		this.cb = cb;
+	}
+	
+	
 }
+
