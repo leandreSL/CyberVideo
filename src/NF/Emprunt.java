9b95ea4c1fcc43983fff199ca7ab5795bd6c0b0a
Antoine Roffet <roffeta@f214-06.e-im2ag.ujf-grenoble.fr>
2019-11-19 16:43:44 +0100

    edit Emprunt

--- src/NF/Emprunt.java
+++ src/NF/Emprunt.java
@@ -7,9 +7,7 @@ import java.util.Date;
 public class Emprunt {
 	long cbEmprunteur;
 	int idAbonne;
-	Date actuelle = new Date();
-	DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-	String dat = dateFormat.format(actuelle);
+	Date date = new Date();
 	Date dateRetour;
 	DVD dvd;
 	
@@ -18,79 +16,58 @@ public class Emprunt {
 		super();
 		this.cbEmprunteur = cbEmprunteur;
 		this.idAbonne = idAbonne;
-		this.dvd = dvd;
+		this.dvd.identifiantDVD = dvd;
 	}
 
+	@Override
+	public String toString() {
+		String chaineRestrictions = String.join("`", Genre.toStringArray(restrictions));
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		String dateD = dateFormat.format(date);
+		String dateR = dateFormat.format(dateRetour);
+		return cbEmprunteur + "|" + idAbonne + "|" + dateD + "|" + dateR + "|" + dvd
+				+ "\n";
+	}
 
 	public long getCbEmprunteur() {
 		return cbEmprunteur;
 	}
 
-
 	public void setCbEmprunteur(long cbEmprunteur) {
 		this.cbEmprunteur = cbEmprunteur;
 	}
-
 
 	public int getIdAbonne() {
 		return idAbonne;
 	}
 
-
 	public void setIdAbonne(int idAbonne) {
 		this.idAbonne = idAbonne;
 	}
 
-
-	public Date getActuelle() {
-		return actuelle;
+	public Date getDate() {
+		return date;
 	}
 
-
-	public void setActuelle(Date actuelle) {
-		this.actuelle = actuelle;
+	public void setDate(Date date) {
+		this.date = date;
 	}
-
-
-	public DateFormat getDateFormat() {
-		return dateFormat;
-	}
-
-
-	public void setDateFormat(DateFormat dateFormat) {
-		this.dateFormat = dateFormat;
-	}
-
-
-	public String getDat() {
-		return dat;
-	}
-
-
-	public void setDat(String dat) {
-		this.dat = dat;
-	}
-
 
 	public Date getDateRetour() {
 		return dateRetour;
 	}
 
-
 	public void setDateRetour(Date dateRetour) {
 		this.dateRetour = dateRetour;
 	}
-
 
 	public DVD getDvd() {
 		return dvd;
 	}
 
-
 	public void setDvd(DVD dvd) {
 		this.dvd = dvd;
 	}
-	
 	
 	
 	
